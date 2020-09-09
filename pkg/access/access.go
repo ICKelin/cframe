f2d3d43991bf6153e6b08d3ec08dc6660a633e56
ICKelin <yingjiu.hulu@gmail.com>
2020-09-09 09:04:15 +0800

    feature: add accesskey manager(draft)

--- pkg/access/access.go
+++ pkg/access/access.go
@@ -1,3 +1,41 @@
 package access
 
-type CloudAccess struct{}
+import (
+	"fmt"
+
+	"github.com/ICKelin/cframe/pkg/etcdstorage"
+)
+
+var (
+	accessPrefix = "/cloud"
+)
+
+type CloudPlatform string
+
+type AccessManager struct {
+	storage *etcdstorage.Etcd
+}
+
+type AccessInfo struct {
+	CloudPlatform CloudPlatform
+	AccessKey     string
+	AccessSecret  string
+}
+
+func NewAccessManager() *AccessManager {
+	return &AccessManager{}
+}
+
+func (m *AccessManager) Add(access *AccessInfo) {
+	key := fmt.Sprintf("%s/%s", accessPrefix, access.CloudPlatform)
+	m.storage.Set(key, access)
+}
+
+func (m *AccessManager) Del(platform CloudPlatform) {
+	key := fmt.Sprintf("%s/%s", accessPrefix, platform)
+	m.storage.Del(key)
+}
+
+func (m *AccessManager) GetAccessList() {
+	// kvs := m.storage.List()
+}
